88b4c7bc4da9f1d0a9b1c5336f3dfe88e5dd9d90
Landon Fuller <landon@landonf.org>
2015-09-26 15:02:38 -0600

    Add Mighty Mouse support from Dirk Engling.
    Patch fetched from: https://reviews.freebsd.org/D3702
    
    I'm not convinced that the FreeBSD BT stack's architecture is ideal;
    especially in terms of having devices fail to enumerate new features
    without first caching their HID details in a "config file" that's
    essentially machine-generated.
    
    This works for now, but bluetooth is a hassle in general. The NetBSD
    stack seems to have a cleaner design (and already supports the Magic
    Mouse); it may be worth investigating replacement of the FreeBSD
    stack with the NetBSD one at a later date.

--- usr.sbin/bluetooth/bthidd/session.c
+++ usr.sbin/bluetooth/bthidd/session.c
@@ -65,6 +65,7 @@ session_open(bthid_server_p srv, hid_device_p const d)
 	memcpy(&s->bdaddr, &d->bdaddr, sizeof(s->bdaddr));
 	s->ctrl = -1;
 	s->intr = -1;
+	s->ctx = NULL;
 
 	if (d->keyboard) {
 		/* Open /dev/vkbdctl */
@@ -175,6 +176,7 @@ session_close(bthid_session_p s)
 			s->srv->maxfd --;
 	}
 
+	free(s->ctx);
 	free(s->keys1);
 	free(s->keys2);
 
