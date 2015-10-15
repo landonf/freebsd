7d945cf36f055c43a10a0074d7f0dedb424b2196
Landon Fuller <landon@landonf.org>
2015-10-14 18:06:57 -0600

    Clean up the address range descriptor constants.

--- sys/dev/bcma/bcma.c
+++ sys/dev/bcma/bcma.c
@@ -37,7 +37,7 @@
 
 #include <dev/bhnd/bhnd.h>
 #include <dev/bhnd/bhndreg.h>
-#include <dev/bhnd/bhnd_core.h>
+#include <dev/bhnd/bhnd_device_ids.h>
 
 #include "bcma.h"
 
@@ -57,29 +57,31 @@
 #define EROM_ENTRY_ISVALID		0x1	/* is entry valid? */
 #define EROM_ENTRY_EOF			0xF	/* end of EROM table */
 
+/* Entry type flags */
 #define EROM_ENTRY_TYPE_MASK		0x6	/* opcode tag mask */
-#  define EROM_ENTRY_CORE		0x0	/* core definition (64-bit) */
+#  define EROM_ENTRY_CORE		0x0	/* core definition */
 #  define EROM_ENTRY_PORT		0x2	/* port definition */
 #  define EROM_ENTRY_ADDR		0x4	/* address (32 or 64-bit) definition */
 
-
+/* First core entry register */
 #define EROM_CORE0_DESIGNER_MASK	0xFFF00000	/* core designer (JEP-106 mfg id) */
 #define EROM_CORE0_DESIGNER_SHIFT	20
 #define EROM_CORE0_ID_MASK		0x000FFF00	/* broadcom-assigned core id */
 #define EROM_CORE0_ID_SHIFT		8
 
-#define EROM_CORE1_MP_MASK		0x0000001F	/* ??? */
-#define EROM_CORE1_MP_SHIFT		4;
-#define EROM_CORE1_SP_MASK		0x00003E00	/* ??? */
-#define EROM_CORE1_SP_SHIFT		9;
-#define EROM_CORE1_WMP_MASK		0x0007C000	/* ??? */
-#define EROM_CORE1_WMP_SHIFT		14
-#define EROM_CORE1_WSP_MASK		0x00F80000	/* ??? */
-#define EROM_CORE1_WSP_SHIFT		19
+/* Second core entry register */
+#define EROM_CORE1_NMP_MASK		0x0000001F	/* count of master port mapping descriptors */
+#define EROM_CORE1_NMP_SHIFT		4;
+#define EROM_CORE1_NSP_MASK		0x00003E00	/* count of slave port mapping descriptors */
+#define EROM_CORE1_NSP_SHIFT		9;
+#define EROM_CORE1_WRAPPER_NMP_MASK	0x0007C000	/* count of wrapper's master port mapping descriptors */
+#define EROM_CORE1_WRAPPER_NMP_SHIFT	14
+#define EROM_CORE1_WRAPPER_NSP_MASK	0x00F80000	/* count of wrapper's slave port mapping descriptors */
+#define EROM_CORE1_WRAPPER_NSP_SHIFT	19
 #define EROM_CORE1_REV_MASK		0xFF000000	/* broadcom-assigned core revision */
 #define EROM_CORE1_REV_SHIFT		24
 
-#define EROM_ADDR_64BIT			0x8	/* address spans two 32-bit opcodes */
+#define EROM_ADDR_64BIT			0x8		/* 64-bit address spans two 32-bit EROM entries */
 
 /**
  * Scan a device enumeration ROM table, adding all discovered cores to the bus.
@@ -91,11 +93,13 @@
  */
 int bcma_scan_erom(device_t bus, struct resource *erom_res, bus_size_t offset, bus_size_t max_size)
 {
-	bus_size_t erom_end = offset + max_size;
+	bus_size_t erom_end;
+	bus_size_t erom_start;
 	
+	erom_end = offset + max_size; // TODO - Refer to ER_REMAPCONTROL offset
+	erom_start = offset;
 	
-	bus_size_t orig_loc = offset;
-	bus_size_t comp_loc = orig_loc;
+	bus_size_t comp_loc = erom_start; // TODO - remove
 
 	while (offset != erom_end) {
 		uint32_t entry = bus_read_4(erom_res, offset);
@@ -104,7 +108,7 @@ int bcma_scan_erom(device_t bus, struct resource *erom_res, bus_size_t offset, b
 			break;
 		
 		if ((entry & EROM_ENTRY_ISVALID) == 0) {
-			device_printf(bus, "erom[0%lx] = 0x%x (marked invalid)\n", (offset-orig_loc)/4, entry);
+			device_printf(bus, "erom[0%lx] = 0x%x (marked invalid)\n", (offset-erom_start)/4, entry);
 			offset += 4;
 			continue;
 		}
@@ -159,30 +163,33 @@ int bcma_scan_erom(device_t bus, struct resource *erom_res, bus_size_t offset, b
 			
 			offset += 4;
 			uint32_t core1 = bus_read_4(erom_res, offset);
-			if ((core1 & EROM_ENTRY_TYPE_MASK) != EROM_ENTRY_CORE)
-				panic("Invalid next entry!");
+			if ((core1 & EROM_ENTRY_TYPE_MASK) != EROM_ENTRY_CORE) {
+				device_printf(bus, "Invalid EROM entry at offset 0x%lx\n", (offset-erom_start));
+				return (EINVAL);
+			}
 			
+			// TODO - Should we skip these?
 			if ((core1 & EROM_ENTRY_ISVALID) == 0)
 				panic("Missing entry IS_VALID flag!");
 			
 			sec_rem = core1 & ~(EROM_ENTRY_ISVALID | EROM_ENTRY_TYPE_MASK);
-			uint8_t mp = (core1 & EROM_CORE1_MP_MASK) >> EROM_CORE1_MP_SHIFT;
-			sec_rem &= ~EROM_CORE1_MP_MASK;
+			uint8_t mp_count = (core1 & EROM_CORE1_NMP_MASK) >> EROM_CORE1_NMP_SHIFT;
+			sec_rem &= ~EROM_CORE1_NMP_MASK;
 			
-			uint8_t sp = (core1 & EROM_CORE1_SP_MASK) >> EROM_CORE1_SP_SHIFT;
-			sec_rem &= ~EROM_CORE1_SP_MASK;
+			uint8_t sp_count = (core1 & EROM_CORE1_NSP_MASK) >> EROM_CORE1_NSP_SHIFT;
+			sec_rem &= ~EROM_CORE1_NSP_MASK;
 
-			uint8_t wmp = (core1 & EROM_CORE1_WMP_MASK) >> EROM_CORE1_WMP_SHIFT;
-			sec_rem &= ~EROM_CORE1_WMP_MASK;
+			uint8_t wmp_count = (core1 & EROM_CORE1_WRAPPER_NMP_MASK) >> EROM_CORE1_WRAPPER_NMP_SHIFT;
+			sec_rem &= ~EROM_CORE1_WRAPPER_NMP_MASK;
 
-			uint8_t wsp = (core1 & EROM_CORE1_WSP_MASK) >> EROM_CORE1_WSP_SHIFT;
-			sec_rem &= ~EROM_CORE1_WSP_MASK;
+			uint8_t wsp_count = (core1 & EROM_CORE1_WRAPPER_NSP_MASK) >> EROM_CORE1_WRAPPER_NSP_SHIFT;
+			sec_rem &= ~EROM_CORE1_WRAPPER_NSP_MASK;
 
-			uint8_t rev = (core1 & EROM_CORE1_REV_MASK) >> EROM_CORE1_REV_SHIFT;
+			uint8_t corerev = (core1 & EROM_CORE1_REV_MASK) >> EROM_CORE1_REV_SHIFT;
 			sec_rem &= ~EROM_CORE1_REV_MASK;
 			
-			device_printf(bus, "erom[%lx] = 0x%x (designer=%s, core=\"%s\" cid=%hx, rev=%hhx)\n", (offset-orig_loc)/4, entry, des, bhnd_core_name(designer, part_num), part_num, rev);
-			device_printf(bus, "erom[%lx] m=%hhx\ts=%hhx\twm=%hhx\tws=%hhx\n", (offset-orig_loc)/4, mp, sp, wmp, wsp);
+			device_printf(bus, "erom[%lx] = 0x%x (designer=%s, core=\"%s\" cid=%hx, rev=%hhx)\n", (offset-erom_start)/4, entry, des, bhnd_core_name(designer, part_num), part_num, corerev);
+			device_printf(bus, "erom[%lx] nmp=%hhx\tnsp=%hhx\tnwmp=%hhx\tnwsp=%hhx\n", (offset-erom_start)/4, mp_count, sp_count, wmp_count, wsp_count);
 
 		} else if (type == EROM_ENTRY_ADDR && entry & EROM_ADDR_64BIT) {
 			remainder &= ~(EROM_ENTRY_ADDR | EROM_ADDR_64BIT);
