1e6e4cf29847d2ff25df579886e00d8ad7a4c58a
Landon Fuller <landon@landonf.org>
2016-03-22 13:41:54 -0600

    Standardize quirk flag matching using the bhnd_device tables.

--- sys/dev/bhnd/cores/pci/bhnd_pci.c
+++ sys/dev/bhnd/cores/pci/bhnd_pci.c
@@ -63,6 +63,15 @@ static int	bhnd_pcie_mdio_cmd_write(struct bhnd_pci_softc *sc,
 static int	bhnd_pcie_mdio_cmd_read(struct bhnd_pci_softc *sc, uint32_t cmd,
 		    uint16_t *data_read);
 
+static struct bhnd_device_quirk bhnd_pci_quirks[];
+static struct bhnd_device_quirk bhnd_pcie_quirks[];
+
+#define BHND_PCI_QUIRKS		bhnd_pci_quirks
+#define BHND_PCIE_QUIRKS	bhnd_pcie_quirks
+#define BHND_PCI_DEV(_core, _desc, ...)				\
+	{ BHND_DEVICE(_core, _desc, BHND_ ## _core ## _QUIRKS,	\
+	    ## __VA_ARGS__), BHND_PCI_REGFMT_ ## _core }
+
 static const struct bhnd_pci_device {
 	uint16_t		 vendor;
 	uint16_t	 	 device;
@@ -70,18 +79,21 @@ static const struct bhnd_pci_device {
 	const char		*pcib_desc;
 	const char		*hostb_desc;
 } bhnd_pci_devs[] = {
-	{ BHND_MFGID_BCM,	BHND_COREID_PCI,	BHND_PCI_REGFMT_PCI,
-	    "Host-PCI bridge",
-	    "PCI-BHND bridge" },
-
-	{ BHND_MFGID_BCM,	BHND_COREID_PCIE,	BHND_PCI_REGFMT_PCIE,
-	    "Host-PCI bridge (PCIe-G1)",
-	    "PCI-BHND bridge (PCIe-G1)" },
+	BHND_PCI_DEV(PCI,	"Host-PCI bridge",		BHND_DF_HOSTB),	     
+	BHND_PCI_DEV(PCI,	"PCI-BHND bridge"),
+	BHND_PCI_DEV(PCIE,	"Host-PCI bridge (PCIe-G1)",	BHND_DF_HOSTB),
+	BHND_PCI_DEV(PCIE,	"PCI-BHND bridge (PCIe-G1)"),
 
 	{ BHND_MFGID_INVALID, BHND_COREID_INVALID, BHND_PCI_REGFMT_PCI,
 	    NULL, NULL }
 };
 
+/* Device quirks tables */
+static struct bhnd_device_quirk bhnd_pci_quirks[] = { BHND_DEVICE_QUIRK_END };
+static struct bhnd_device_quirk bhnd_pcie_quirks[] = {
+	{ BHND_HWREV_GTE	(10),	BHND_PCI_QUIRK_SD_C22_EXTADDR },
+	BHND_DEVICE_QUIRK_END
+};
 
 #define	BHND_PCIE_MDIO_CTL_DELAY	10	/**< usec delay required between
 						  *  MDIO_CTL/MDIO_DATA accesses. */
@@ -145,8 +157,8 @@ bhnd_pci_generic_attach(device_t dev)
 
 	sc = device_get_softc(dev);
 	sc->dev = dev;
-	
-	// TODO: Quirk matching
+	sc->quirks = bhnd_device_quirks(dev, &bhnd_pci_devs[0].device,
+	    sizeof(bhnd_pci_devs[0]));
 
 	/* Allocate bus resources */
 	sc->mem_res = bhnd_alloc_resource_any(dev, SYS_RES_MEMORY, &sc->mem_rid,
