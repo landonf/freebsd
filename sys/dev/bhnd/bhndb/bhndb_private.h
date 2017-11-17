90223bda63240dbc7ff9f71d3f4110cf7c5479b6
Landon Fuller <landonf@FreeBSD.org>
2017-11-17 15:05:36 -0700

    Add support for attaching allocation flags to bhndb_resource and bhndb_port_priority entries, and define a new BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT allocation flags that we'll use to determine whether we should steal a register window to fulfill indirect resource requests.

--- sys/dev/bhnd/bhndb/bhndb_private.h
+++ sys/dev/bhnd/bhndb/bhndb_private.h
@@ -70,6 +70,7 @@ int				 bhndb_add_resource_region(
 				     struct bhndb_resources *br,
 				     bhnd_addr_t addr, bhnd_size_t size,
 				     bhndb_priority_t priority,
+				     uint32_t alloc_flags,
 				     const struct bhndb_regwin *static_regwin);
 
 int				 bhndb_find_resource_limits(
@@ -128,6 +129,12 @@ const struct bhndb_hw_priority	*bhndb_hw_priority_find_core(
 				     const struct bhndb_hw_priority *table,
 				     struct bhnd_core_info *core);
 
+const struct bhndb_port_priority *bhndb_hw_priorty_find_port(
+				     const struct bhndb_hw_priority *table,
+				     struct bhnd_core_info *core,
+				     bhnd_port_type port_type, u_int port,
+				     u_int region);
+
 
 /**
  * Dynamic register window allocation reference.
@@ -156,6 +163,7 @@ struct bhndb_region {
 	bhnd_addr_t			 addr;		/**< start of mapped range */
 	bhnd_size_t			 size;		/**< size of mapped range */
 	bhndb_priority_t		 priority;	/**< direct resource allocation priority */
+	uint32_t			 alloc_flags;	/**< resource allocation flags (@see bhndb_alloc_flags) */
 	const struct bhndb_regwin	*static_regwin;	/**< fixed mapping regwin, if any */
 
 	STAILQ_ENTRY(bhndb_region)	 link;
