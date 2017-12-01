c0633c4ca10c16b4967d4237973980808c9ac5c5
Landon Fuller <landonf@FreeBSD.org>
2017-12-01 16:40:53 -0700

    Revert "Eliminate kmem_arena and kmem_object in preparation for further NUMA commits."
    This reverts commit 990ca74cdcd5337d23a259833de13c1ce7f00d48.

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -239,22 +239,16 @@ sysctl_kmem_map_size(SYSCTL_HANDLER_ARGS)
 {
 	u_long size;
 
-	size = uma_size();
+	size = vmem_size(kmem_arena, VMEM_ALLOC);
 	return (sysctl_handle_long(oidp, &size, 0, req));
 }
 
 static int
 sysctl_kmem_map_free(SYSCTL_HANDLER_ARGS)
 {
-	u_long size, limit;
+	u_long size;
 
-	/* The sysctl is unsigned, implement as a saturation value. */
-	size = uma_size();
-	limit = uma_limit();
-	if (size > limit)
-		size = 0;
-	else
-		size = limit - size;
+	size = vmem_size(kmem_arena, VMEM_FREE);
 	return (sysctl_handle_long(oidp, &size, 0, req));
 }
 
@@ -675,6 +669,19 @@ reallocf(void *addr, unsigned long size, struct malloc_type *mtp, int flags)
 	return (mem);
 }
 
+/*
+ * Wake the uma reclamation pagedaemon thread when we exhaust KVA.  It
+ * will call the lowmem handler and uma_reclaim() callbacks in a
+ * context that is safe.
+ */
+static void
+kmem_reclaim(vmem_t *vm, int flags)
+{
+
+	uma_reclaim_wakeup();
+	pagedaemon_wakeup();
+}
+
 #ifndef __sparc64__
 CTASSERT(VM_KMEM_SIZE_SCALE >= 1);
 #endif
@@ -752,7 +759,9 @@ kmeminit(void)
 #else
 	tmp = vm_kmem_size;
 #endif
-	uma_set_limit(tmp);
+	vmem_init(kmem_arena, "kmem arena", kva_alloc(tmp), tmp, PAGE_SIZE,
+	    0, 0);
+	vmem_set_reclaim(kmem_arena, kmem_reclaim);
 
 #ifdef DEBUG_MEMGUARD
 	/*
@@ -760,7 +769,7 @@ kmeminit(void)
 	 * replacement allocator used for detecting tamper-after-free
 	 * scenarios as they occur.  It is only used for debugging.
 	 */
-	memguard_init(kernel_arena);
+	memguard_init(kmem_arena);
 #endif
 }
 
