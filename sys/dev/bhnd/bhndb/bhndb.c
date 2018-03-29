ad4ac9f00b9746ae9e9c572b663726bb42cc2dfb
Landon Fuller <landonf@FreeBSD.org>
2018-03-29 13:20:13 -0600

    Clean up branch for initial commit; drop DMA64-specific changes.

--- sys/dev/bhnd/bhndb/bhndb.c
+++ sys/dev/bhnd/bhndb/bhndb.c
@@ -2128,14 +2128,7 @@ bhndb_get_dma_translation(device_t dev, device_t child, u_int width,
 	if (sc->bus_res->res->dma_tags == NULL)
 		return (ENODEV);
 
-	/* Is the requested width supported? */
-	if (width > BHND_DMA_ADDR_32BIT) {
-		/* Backplane must support 64-bit addressing */
-		if (!(sc->chipid.chip_caps & BHND_CAP_BP64))
-			width = BHND_DMA_ADDR_32BIT;
-	}
-
-	/* Find the best matching descriptor for the requested width */
+	/* Find the best matching descriptor for the requested type */
 	addr_mask = BHND_DMA_ADDR_BITMASK(width);
 
 	match = NULL;
